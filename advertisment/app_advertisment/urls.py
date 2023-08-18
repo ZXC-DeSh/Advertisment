2ed38f2a568ce28d7a789aa1f7c17dd14a2f2597
ZXC_DeSh <egorkorobkin37@gmail.com>
2023-08-18 12:50:42 +0300

    git commit -m "yrok 18.08"

--- advertisment/app_advertisment/urls.py
+++ advertisment/app_advertisment/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index, top_sellers
+from .views import index, top_sellers, advertisement_post, profile, login, register
 
 urlpatterns = [
     path('', index, name='main_page'),
-    path('top-sellers/', top_sellers, name='top_sellers')
+    path('top-sellers/', top_sellers, name='top_sellers'),
+    path('advertisement_post/', advertisement_post, name='adv_post'),
+    path('profile/', profile, name='profile'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
--- advertisment/app_advertisment/views.py
+++ advertisment/app_advertisment/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Advertisement
+from .forms import AdvertisementForm
+from django.urls import reverse
 
 
 def index(request):
@@ -7,6 +9,30 @@ def index(request):
     context = {'advertisements': advertisements}
     return render(request, 'index.html', context)
 
-
 def top_sellers(request):
     return render(request, 'top-sellers.html')
+
+def advertisement_post(request):
+    if request.method == 'POST':
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main_page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+
+    form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement_post.html', context)
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
