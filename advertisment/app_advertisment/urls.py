e88707ce4f26132f2c186315c3fda24be2aefe7a
ZXC_DeSh <egorkorobkin37@gmail.com>
2023-08-23 16:46:47 +0300

    git commit -m"login and logout"

--- advertisment/app_advertisment/urls.py
+++ advertisment/app_advertisment/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     path('', index, name='main_page'),
     path('top-sellers/', top_sellers, name='top_sellers'),
     path('advertisement_post/', advertisement_post, name='adv_post'),
-    path('profile/', profile, name='profile'),
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
 ]
--- advertisment/app_advertisment/views.py
+++ advertisment/app_advertisment/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Advertisement
+from django.contrib.auth.decorators import login_required
 from .forms import AdvertisementForm
-from django.urls import reverse
+from django.urls import reverse,reverse_lazy
 
 
 def index(request):
     advertisements = Advertisement.objects.all()
     context = {'advertisements': advertisements}
-    return render(request, 'index.html', context)
+    return render(request, 'adv/index.html', context)
 
 def top_sellers(request):
-    return render(request, 'top-sellers.html')
+    return render(request, 'adv/top-sellers.html')
 
+@login_required(login_url=reverse_lazy('login'))
 def advertisement_post(request):
     if request.method == 'POST':
         form = AdvertisementForm(request.POST, request.FILES)
@@ -26,13 +28,13 @@ def advertisement_post(request):
 
     form = AdvertisementForm()
     context = {'form': form}
-    return render(request, 'advertisement_post.html', context)
+    return render(request, 'adv/advertisement_post.html', context)
 
 def register(request):
-    return render(request, 'register.html')
+    return render(request, 'auth/register.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'auth/login.html')
 
 def profile(request):
-    return render(request, 'profile.html')
+    return render(request, 'auth/profile.html')
